11961d216e92dd4a5c4bbe7e0a347a1b5cf6f91e
mathcard <matheus.carmo@hotmail.com>
2017-10-23 17:51:43 -0300

    Arquivos php de inserção, e login

---
+++ inserção/T01in.php
@@ -0,0 +1,15 @@
+<?php
+require "connect.php";
+$nome = $_POST['ct_nome'];
+$descricao = $_POST['ct_desc'];
+$vida = $_POST['ct_vida'];
+
+$sql = "insert into Categoria values (DEFAULT,?,?,?)";
+$resultado = $con->prepare($sql);
+$resultado->bindParam(1, $nome);
+$resultado->bindParam(2, $descricao);
+$resultado->bindParam(3, $vida);
+$resultado->execute();
+header ("location: index.php");
+
+?>
---
+++ inserção/T04in.php
@@ -0,0 +1,12 @@
+<?php
+require "connect.php";
+$sig = $_POST['dp_sigla'];
+$nome = $_POST['dp_nome'];
+
+$sql = "insert into Departamento values (?,?)";
+$resultado = $con->prepare($sql);
+$resultado->bindParam(1, $sig);
+$resultado->bindParam(2, $nome);
+$resultado->execute();
+header ("location: index.php");
+?>
