2dbabd6d8ff97ee0cd472545a07a898c64b87d22
mathcard <matheus.carmo@hotmail.com>
2017-10-26 10:20:15 -0300

    08 falta as validações de usuario, 07 faltas as validações, e arrumar o jason da sala de origem

--- T07in.php
+++ T07in.php
@@ -3,6 +3,7 @@ require "connect.php";
 require "verifica.php";
 $LOGIN = $_SESSION['login'];
 $bem = $_POST['selbem'];
+$org = $_POST['selsalao'];
 $dest = $_POST['selsalad'];
 
 ### Sql buscar o ID solicitante ##
@@ -10,28 +11,14 @@ $sql2 = $con->query("select id from usuario where login = '$LOGIN'");
 $row = $sql2->fetch(PDO::FETCH_OBJ);
 $id = $row->id;
 
-
-### Sql busca o ID da sala, do BEM selecionado ###
-$sql3 = $con->prepare("select s.numero as nsala from sala s inner join bempatrimonial b on s.numero = b.numsala where b.numero = :bem");
-$sql3->bindValue(":bem", $bem, PDO::PARAM_INT);
-$sql3->execute();
-    $total = $sql3->rowCount();
-            while ($row = $sql3->fetchObject()) {
-                $origem=$row->nsala;
-        #echo $origem;
-        }
-
 ### Sql de inserção na tabela MBP ###
-$sql = "INSERT into MBP (numero,dataSolicitacao,motivo,idSolicitante,numeroBem,numSalaOrigem,numSalaDestino) values (DEFAULT, current_date,?,'$id',?,'$origem',?)";
+$sql = "INSERT into MBP (numero,dataSolicitacao,motivo,idSolicitante,numeroBem,numSalaOrigem,numSalaDestino) values (DEFAULT, current_date,?,'$id',?,?,?)";
 $resultado = $con->prepare($sql);
 $resultado->bindParam(1, $_POST['motivo']);
 $resultado->bindParam(2, $_POST['selbem']);
-$resultado->bindParam(3, $_POST['selsalad']);
+$resultado->bindParam(3, $_POST['selsalao']);
+$resultado->bindParam(4, $_POST['selsalad']);
 $resultado->execute();
 
-### Sql alterando a sala origem do BEM ###
-$sql4 =$con->prepare("update bempatrimonial set numsala = ? where numero = ?");
-$sql4->execute(array($dest, $bem));
 header ("location: index.php");
 ?>
-
--- T08aut.php
+++ T08aut.php
@@ -1,16 +1,41 @@
+
 <?php
 require "connect.php";
 require "verifica.php";
-$LOGIN = $_SESSION['login'];
-$bem = $_GET['numero'];
+$LOGIN = $_SESSION['login']; #usado para buscar o usuario conectado
+$bem = $_GET['numero']; #numero da MBP
 
-### Sql buscar o ID solicitante ##
+### Sql buscar o ID autorizador ##
 $sql = $con->query("select id from Usuario where login = '$LOGIN'");
 $row = $sql->fetch(PDO::FETCH_OBJ);
 $id = $row->id;
 
-##Sql inserção do autorizador ###
+### SQL  atual
+$sqlD = $con->query("select date from current_date");
+$row = $sqlD->fetch(PDO::FETCH_OBJ);
+$data = $row->date;
+
+### SQL Horario atual
+$sqlH = $con->query("select timetz from current_time");
+$row = $sqlH->fetch(PDO::FETCH_OBJ);
+$hora = $row->timetz;
+
+### SQl buscar sala destino e bem
+$sqlS = $con->query("select numsaladestino, numerobem from mbp where numero = '$bem'");
+$row = $sqlS->fetch(PDO::FETCH_OBJ);
+$sala = $row->numsaladestino;
+$numbem = $row->numerobem;
+
+
+###Sql inserção do autorizador ###
 $sql2 = $con->prepare("update MBP set dataconfirmacao = ?, horaconfirmacao = ?, idautorizador = ? where numero = ? ");
-$sql2->execute(array("current_date", "current_time" ,$id, $bem));
-#header ("location: T08.php");
+$sql2->execute(array($data,$hora,$id,$bem));
+
+### Sql alterando a sala do BEM ###
+$sqlB =$con->prepare("update bempatrimonial set numsala = ? where numero = ?");
+$sqlB->execute(array($sala, $numbem));
+
+header ("location: T08.php");
+
 ?>
+
