384a327f8112609e2de799fb0d9a2dc366196b68
mathcard <matheus.carmo@hotmail.com>
2017-10-30 18:29:46 -0300

    connect

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
 <?php
-$con = new PDO('pgsql:dbname=patrimonio;host=localhost;user=postgres;password=postgres');
+$con = new PDO('pgsql:dbname=patrimonio;host=192.168.41.53;user=queops;password=piramide');
 ?>
+
