6ad3ad0b31d1474ce167ce33100b9775512bbc61
cristrauss <31821185+cristrauss@users.noreply.github.com>
2017-11-12 11:25:53 -0200

    pdf
    gerador de pdf, falta corrigir os selects

---
+++ pdf.php
@@ -0,0 +1,60 @@
+<?php
+require('fpdf/fpdf.php');
+require "connect.php"; 
+
+class Documento1 extends FPDF
+{
+    function Header()
+    {
+        $this->Image('logor.png', 340, 0);
+        $this->Ln(90);
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(800, 20, 'Inventario', 1,1, 'C');
+        $this->Cell(60, 20, 'Numero', 1,0, 'C');
+        $this->Cell(130, 20, 'Descricao', 1,0, 'C');
+        $this->Cell(80, 20, 'Data Compra', 1,0, 'C');
+        $this->Cell(60, 20, 'Garantia', 1,0, 'C');
+        $this->Cell(60, 20, 'Nota', 1,0, 'C');
+        $this->Cell(150, 20, 'Fornecedor', 1,0, 'C');
+        $this->Cell(80, 20, 'Valor', 1,0, 'C');
+        $this->Cell(60, 20, 'Situacao', 1,0, 'C');
+        $this->Cell(60, 20, 'Categoria', 1,0, 'C');
+        $this->Cell(60, 20, 'Sala', 1,0, 'C');
+    
+        $this->Ln(20);
+    }
+    function Footer()
+    {
+        $this->SetY(-15);
+        $this->SetFont('Arial', 'B', 8);
+        $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
+
+    }
+}
+
+$pdf = new Documento1('P', 'pt', 'A4');
+$pdf->AddPage('L');
+$pdf->AliasNbPages();
+$sql= "SELECT * FROM bempatrimonial ORDER BY numero ";
+$resultado = $con->prepare($sql);
+$resultado->execute();
+
+
+
+while ($row = $resultado->fetchObject()) {
+
+    $pdf->Cell(60, 20, $row->numero, 1, 0, 'L'); 
+    $pdf->Cell(130, 20, $row->descricao, 1, 0, 'L'); 
+    $pdf->Cell(80, 20, $row->datacompra, 1, 0, 'L'); 
+    $pdf->Cell(60, 20, $row->prazogarantia, 1, 0, 'L'); 
+    $pdf->Cell(60, 20, $row->nrnotafiscal, 1, 0, 'L'); 
+    $pdf->Cell(150, 20, $row->fornecedor, 1, 0, 'L'); 
+    $pdf->Cell(80, 20, $row->valor, 1, 0, 'L'); 
+    $pdf->Cell(60, 20, $row->situacao, 1, 0, 'L'); 
+    $pdf->Cell(60, 20, $row->codcategoria, 1, 0, 'L'); 
+    $pdf->Cell(60, 20, $row->numsala, 1, 1, 'L'); 
+
+}
+
+$pdf->Output();
+?>
