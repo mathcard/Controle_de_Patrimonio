f83c51de247d49340adae50c42828f7c996f4015
mathcard <matheus.carmo@hotmail.com>
2017-11-13 19:02:46 -0300

    validação modelo

--- T01in.php
+++ T01in.php
@@ -1,35 +1,48 @@
 <?php
-require "connect.php";
-require "onlyP.php";
 
+require "connect.php";
+require "verifica.php";
+require "onlyP.php";
+require "modelo2.php";
 $nome = $_POST['ct_nome'];
 $descricao = $_POST['ct_desc'];
 $vida = $_POST['ct_vida'];
-
-/*
-if(!$nome){
-echo " É necessario informar o NOME.";
+echo "<br><br>";
+$aux = true;
+if(empty($nome)){
+	echo " É necessario informar o <b>NOME.</b> <br> ";
+	$aux = false;
 }elseif (strlen($nome) > 50) {
-echo "O NOME tem Limite de 50 caracteres";
+	echo "O <b>NOME</b> não pode exceder 50 caracteres. <br>";
+	$aux = false;
 }
-if(!$descricao){
-echo "É necessario informar a DESCRIÇÃO";
-}elseif (strlen($descricao)>400){
-echo "A DESCRIÇÃO possui limite de 400 caracteres";
-}
-if(!$vida){
-echo "É necessario infomrar a VIDA ÚTIL";
-}elseif($vida <= 0){
-echo "Esse valor é invalido";
-}
-header ("refresh:3; url=T01f.php");
-*/
 
-$sql = "insert into Categoria values (DEFAULT,?,?,?)";
-$resultado = $con->prepare($sql);
-$resultado->bindParam(1, $nome);
-$resultado->bindParam(2, $descricao);
-$resultado->bindParam(3, $vida);
-$resultado->execute();
-header ("refresh:3; url=index.php");
+if(empty($descricao)){
+	echo "É necessario informar a <b>DESCRIÇÃO</b> <br>";
+	$aux = false;
+}elseif (strlen($descricao)>400){
+	echo "A <b>DESCRIÇÃO</b> não pode exceder 400 caracteres <br>";
+	$aux = false;
+}
+
+if(empty($vida)){
+	echo "É necessario infomrar a <b>VIDA ÚTIL</b> <br>";
+	$aux = false;
+}elseif($vida <= 0){
+	echo "<b>Vida Útil</b>  inválida <br>";
+	$aux = false;
+}
+	if($aux!=true){
+		header ("refresh:5; url=T01f.php");
+	}else{
+	$sql = "insert into Categoria values (DEFAULT,?,?,?)";
+	$resultado = $con->prepare($sql);
+	$resultado->bindParam(1, $nome);
+	$resultado->bindParam(2, $descricao);
+	$resultado->bindParam(3, $vida);
+	$resultado->execute();
+	echo "Categoria incluida com sucesso!!!";
+	header ("refresh:5; url=T01f.php");
+
+}
 ?>
