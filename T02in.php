f320a1821e4b07f234442cbcbd4f35140118117a
mathcard <matheus.carmo@hotmail.com>
2017-11-13 19:59:12 -0300

    validação 'final'

--- T02in.php
+++ T02in.php
@@ -1,65 +1,88 @@
 <?php
 require "connect.php";
+require "verifica.php";
 require "onlyP.php";
-/*
-if(!$_POST['pr_nome']){
-echo "É necessario infomrar o NOME.";
+require "modelo2.php";
+echo "<br><br>";
+$aux = true;
+if(empty($_POST['pr_nome'])){
+	echo "É necessario informar o <b>NOME.</b><br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_nome']) > 50){
-echo "O NOME TEM LIMITE de 50 caracteres";
+	echo "O <b>NOME</b> não pode exceder 50 caracteres<br>";
+	$aux = false;
 }
 
-if(!$_POST['pr_cep']){
-echo "É necessario infomrar o CEP.";
+if(empty($_POST['pr_cep'])){
+	echo "É necessario informar o <b>CEP</b>.<br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_cep']) != 8){
-echo "O CEP deve possui 8 caracteres";
+	echo "O <b>CEP</b> deve possui 8 caracteres<br>";
+	$aux = false;
 }
 
-if(!$_POST['pr_rua']){
-echo "É necessario infomrar o NOME.";
+if(empty($_POST['pr_rua'])){
+	echo "É necessario informar o <b>RUA.</b><br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_rua']) > 60){
-echo "A RUA TEM LIMITE de 60 caracteres";
+	echo "A <b>RUA</b> não deve exceder 60 caracteres. <br>";
+	$aux = false;
 }
 
-if(!$_POST['pr_comp']){
-echo "É necessario infomrar o COMPLMENTO.";
+if(empty($_POST['pr_comp'])){
+	echo "É necessario infomrar o <b>COMPLMENTO.</b><br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_comp']) > 10){
-echo "O COMPLMENTO TEM LIMITE de 10 caracteres";
+	echo "O <b>COMPLMENTO</b> não deve exceder 10 caracteres. <br>";
+	$aux = false;
 }
 
-if(!$_POST['pr_num']){
-echo "É necessario infomrar o NUMERO.";
+if(empty($_POST['pr_num'])){
+	echo "É necessario infomrar o <b>NUMERO.</b><br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_num']) > 10){
-echo "O NÚMERO TEM LIMITE de 10 caracteres";
+	echo "O <b>NÚMERO</b> não deve exceder 10 caracteres.<br>";
+	$aux = false;
 }
 
-if(!$_POST['pr_bairro']){
-echo "É necessario infomrar o BAIRRO.";
+if(empty($_POST['pr_bairro'])){
+	echo "É necessario infomrar o <b>BAIRRO</b>.<br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_bairro']) > 40){
-echo "O BAIRRO TEM LIMITE de 40 caracteres";
+	echo "O <b>BAIRRO</b> não deve exceder 40 caracteres.<br>";
+	$aux = false;
 }
 
-if(!$_POST['pr_city']){
-echo "É necessario infomrar a CIDADE.";
+if(empty($_POST['pr_city'])){
+	echo "É necessario infomrar a <b>CIDADE</b>.<br>";
+	$aux = false;
 }elseif (strlen($_POST['pr_city']) > 50){
-echo "A CIDADE TEM LIMITE de 50 caracteres";
+	echo "A <b>CIDADE</b> não deve exceder 50 caracteres.<br>";
+	$aux = false;
 }
 
-if (strlen($_POST['estado']) > 2){
-echo "O ESTADO TEM LIMITE de 2 caracteres";
+if(empty($_POST['estado'])){
+	echo "É necessario informar a <b>ESTADO</b>.<br>";
+	$aux = false;
+}elseif(strlen($_POST['estado']) > 2){
+	echo "O <b>ESTADO</b> não deve exceder 2 caracteres.<br>";
+	$aux = false;
 }
-header ("refresh:13; url=T02f.php");
-*/
-
-$sql = "insert into Predio values (DEFAULT,?,?,?,?,?,?,?,?)";
-$resultado = $con->prepare($sql);
-$resultado->bindParam(1, $_POST['pr_nome']);
-$resultado->bindParam(2, $_POST['pr_cep']);
-$resultado->bindParam(3, $_POST['pr_rua']);
-$resultado->bindParam(4, $_POST['pr_comp']);
-$resultado->bindParam(5, $_POST['pr_num']);
-$resultado->bindParam(6, $_POST['pr_bairro']);
-$resultado->bindParam(7, $_POST['pr_city']);
-$resultado->bindParam(8, $_POST['estado']);
-$resultado->execute();
-header ("location: index.php");
+	if($aux != true){
+		header ("refresh:5; url=T02f.php");
+	}else{
+	$sql = "insert into Predio values (DEFAULT,?,?,?,?,?,?,?,?)";
+	$resultado = $con->prepare($sql);
+	$resultado->bindParam(1, $_POST['pr_nome']);
+	$resultado->bindParam(2, $_POST['pr_cep']);
+	$resultado->bindParam(3, $_POST['pr_rua']);
+	$resultado->bindParam(4, $_POST['pr_comp']);
+	$resultado->bindParam(5, $_POST['pr_num']);
+	$resultado->bindParam(6, $_POST['pr_bairro']);
+	$resultado->bindParam(7, $_POST['pr_city']);
+	$resultado->bindParam(8, $_POST['estado']);
+	$resultado->execute();
+        echo "Predio incluido com sucesso!!!";
+	header ("refresh:5; url=T02f.php");
+	}
 ?>
