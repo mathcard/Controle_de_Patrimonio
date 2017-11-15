c62d3d4c3e59e5019fdc78dac4a322361fc39325
cristrauss <31821185+cristrauss@users.noreply.github.com>
2017-11-15 16:25:52 -0200

    t11nova

--- T11.php
+++ T11.php
@@ -1,11 +1,70 @@
+
+
+<script type='text/javascript'>
+
+(function()
+{
+  if( window.localStorage )
+  {
+    if( !localStorage.getItem( 'firstLoad' ) )
+    {
+      localStorage[ 'firstLoad' ] = true;
+      window.location.reload();
+    }  
+    else
+      localStorage.removeItem( 'firstLoad' );
+  }
+})();
+
+</script>
+
 <?php 
 require "modelo.php";
 require "connect.php"; 
+
+/*
+if (!empty($_GET['selpredio'])) {
+    if (isset($_GET['seldep'])) {
+        if (isset($_GET['selsala'])) {
+            $inner=", sala, predio, departamento ";
+            $select=" and bempatrimonial.numsala=sala.numero and sala.numero={$_GET['selsala']}";
+        }
+    $inner=", sala, departamento";
+    $select=" and bempatrimonial.numsala=sala.numero and sala.sigladpto=departamento.sigla and departamento.sigla={$_GET['seldep']}";
+    }
+$inner=", sala, predio, departamento ";
+$predio=" and bempatrimonial.numsala=sala.numero and sala.codpredio=predio.codigo and predio.codigo={$_GET['selpredio']}";
+}else{
+$inner="";
+$predio="";
+}
+
+*/
 if (isset($_GET['ordem'])) {
     $ordem=" ORDER BY " . $_GET['ordem'];
 }else {
     $ordem="";
 }
+
+
+    if (!empty($_GET['data'])){
+        echo"is empty";
+    $data10=$_GET['data'];
+    $mm=' < ';
+
+}else {
+    $sql= "SELECT date FROM current_date";
+    $resultado = $con->prepare($sql);
+    $resultado->execute();
+    $row = $resultado->fetchObject();
+    $data10=$row->date;
+    $mm=' > ';
+
+}
+
+#$data2=",baixabempatrimonial where datacompra<" . $data . "'2000-10-16' AND baixabempatrimonial.data<'" . $data . "'";
+#$data1=" and datacompra<" . $data . "'2000-10-16' AND baixabempatrimonial.data<'" . $data . "'";
+#$meio=", baixabempatrimonial ";
 
 if (isset($_GET['nome'])) {
    setcookie('aux',$_GET['nome'], time() + 30);
@@ -25,8 +84,8 @@ if (isset($_COOKIE['aux'])){
 
 ?>
     <div style="margin-left:33%;padding:70px 0">
-        <div class="logo">Buscar Bem</div>
-        <!-- Main Form -->
+        <div class="logo" style="margin-left: 5cm;">Buscar Bem</div>
+
         <div class="login-form-1">
             <form id="login-form" class="text-left" action="T11.php" method="get">
                 <div style="width:500px" class="main-login-form">
@@ -35,16 +94,21 @@ if (isset($_COOKIE['aux'])){
                             <label for="nome" class="sr-only">Nome</label>
                             <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome">
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <label for="data" class="sr-only">Nome</label>
+                            <input type="date" class="form-control" id="data" name="data">
+                        </div>
+                    </div>                    
                     <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
                 </div>
                 <div class="etc-login-form">
                     <a href="T06.php">Incluir Bem</a>
                 </div>
+                
             </form>
         </div>
     </div>
-    <!-- end:Main Form -->
+
     <div id="main" class="container-fluid">
 	</div>
     <div class='table-responsive col-md-12'>
@@ -62,6 +126,7 @@ if (isset($_COOKIE['aux'])){
                     echo "<th><a href='T11.php?ordem=situacao{$pnome}'>Situação</a></th>";
                     echo "<th><a href='T11.php?ordem=codcategoria{$pnome}'>Categoria</a></th>";
                     echo "<th><a href='T11.php?ordem=numsala{$pnome}'>Sala</a></th>";
+                    echo "<th><a href='#'>Depreciaçao</a></th>";
                     echo "<th class='actions text-center'>Ação</th>";
                     ?>
                 </tr>
@@ -70,65 +135,67 @@ if (isset($_COOKIE['aux'])){
 	<tbody>
 	
             <?php
-if(isset($_GET['nome'])){
-    #$nome = "%".$_GET['nome']."%";
-    $nome = $_GET['nome'];
-    $sql= "SELECT * FROM bempatrimonial WHERE descricao LIKE :nome" . $ordem;
+                                               
+
+if(!empty($_GET['nome'])){
+
+    $nome = "%" . $_GET['nome'] . "%";
+    echo $nome . "  ";
+   // $sql= "SELECT * FROM bempatrimonial " . $meio . " WHERE upper(descricao) LIKE upper(:nome)". $data1 . $ordem;
+    $sql= "SELECT * FROM bempatrimonial where numero in
+    ((select b.numero from bempatrimonial b where b.situacao = 'I' and b.datacompra <= '{$data10}')
+    union all
+    (select ba.numero from baixabempatrimonial ba where ba.data {$mm} '{$data10}')) and upper(descricao) like upper('{$nome}')" . $ordem;
     $resultado = $con->prepare($sql);
-    $resultado->bindParam(':nome', $nome, PDO::PARAM_STR);
+   // $resultado->bindParam(':nome', $nome, PDO::PARAM_STR);
     $resultado->execute();
-    $total = $resultado->rowCount();
-    while ($row = $resultado->fetchObject()) {
-                $id=$row->numero;
-                echo "<tr>";          
-		echo "<td><b>{$row->numero}</b></td>";
-		echo "<td><b>{$row->descricao}</b></td>";
-                echo "<td><b>{$row->datacompra}</b></td>";
-                echo "<td><b>{$row->prazogarantia}</b></td>";
-                echo "<td><b>{$row->nrnotafiscal}</b></td>";
-                echo "<td><b>{$row->fornecedor}</b></td>";
-                echo "<td><b>{$row->valor}</b></td>";
-                echo "<td><b>{$row->situacao}</b></td>";
-                echo "<td><b>{$row->codcategoria}</b></td>";
-                echo "<td><b>{$row->numsala}</b></td>";
-                echo "<td>
-		 <a href='baixa.php?id=$id'>
-                <input type='button' name='insert' value='Apagar' />
-                </a>";
-                echo "<a href='alterabem.php?id=$id'>
-                       <input type='button' name='insert' value='Editar' />
-                       </a></td>";
-                echo "</tr>";
-                    }
+    #$total = $resultado->rowCount();
+
 }else{
-            $sql= "SELECT * FROM bempatrimonial" . $ordem;
-            $resultado = $con->prepare($sql);
-           # $resultado->bindParam(':ordem', $ordem, PDO::PARAM_STR);
+            ###
+            $sqlX= "SELECT * FROM bempatrimonial where numero in
+            ((select b.numero from bempatrimonial b where b.situacao = 'I' and b.datacompra <= '{$data10}')
+            union all
+            (select ba.numero from baixabempatrimonial ba where ba.data {$mm} '{$data10}'))" . $ordem;
+            $resultado = $con->prepare($sqlX);
             $resultado->execute();
-            $total = $resultado->rowCount();
-            while ($row = $resultado->fetchObject()) {
-                echo "<tr>";
-                echo "<td><b>{$row->numero}</b></td>";
-                echo "<td><b>{$row->descricao}</b></td>";
-                echo "<td><b>{$row->datacompra}</b></td>";
-                echo "<td><b>{$row->prazogarantia}</b></td>";
-                echo "<td><b>{$row->nrnotafiscal}</b></td>";
-                echo "<td><b>{$row->fornecedor}</b></td>";
-                echo "<td><b>{$row->valor}</b></td>";
-                echo "<td><b>{$row->situacao}</b></td>";
-                echo "<td><b>{$row->codcategoria}</b></td>";
-                echo "<td><b>{$row->numsala}</b></td>";
-		$id2=$row->numero;
-                echo "<td>
-		<a href='baixa.php?id=$id2'>
-		<input type='button' name='insert' value='Apagar' />
-        </a>";
-        echo "<a href='alterabem.php?id=$id2'>
-		<input type='button' name='insert' value='Editar' />
-		</a></td>";
-                echo "</tr>";
+
+            ###
+           # $sql= "SELECT * FROM bempatrimonial " . $data2 . $ordem;
+           # $resultado = $con->prepare($sql);
+           # $resultado->execute();
+            #$total = $resultado->rowCount();
             }
-        }
+        
+
+        while ($row = $resultado->fetchObject()) {
+            $id=$row->numero;
+            echo "<tr>";          
+            echo "<td><b>{$row->numero}</b></td>";
+            echo "<td><b>{$row->descricao}</b></td>";
+            echo "<td><b>{$row->datacompra}</b></td>";
+            echo "<td><b>{$row->prazogarantia}</b></td>";
+            echo "<td><b>{$row->nrnotafiscal}</b></td>";
+            echo "<td><b>{$row->fornecedor}</b></td>";
+            echo "<td><b>{$row->valor}</b></td>";
+            echo "<td><b>{$row->situacao}</b></td>";
+            echo "<td><b>{$row->codcategoria}</b></td>";
+            echo "<td><b>{$row->numsala}</b></td>";
+            $sql2="select round(valor/vidautil, 2) as depre from bempatrimonial inner join categoria on codcategoria=codigo and numero=" . $row->numero;
+            $resultado2 = $con->prepare($sql2);
+            $resultado2->execute();
+            $row2 = $resultado2->fetchObject();
+                echo "<td><b>{$row2->depre} a/m</b></td>";
+            
+            echo "<td>
+     <a href='baixa.php?id=$id'>
+            <input type='button' name='insert' value='Apagar' />
+            </a>";
+            echo "<a href='alterabem.php?id=$id'>
+                   <input type='button' name='insert' value='Editar' />
+                   </a></td>";
+            echo "</tr>";
+                }
             ?>
     </tbody>
     </table>
@@ -137,7 +204,7 @@ if(isset($_GET['nome'])){
         <a href="index.php">Voltar</a>        
         <a href="T11.php" onClick="SetCookies('aux','','-1')">Listar novamente</a>
     </div>
-    <!-- end:Main Form -->
+
 </body>
 <script>
 function SetCookies(c_name,value,expiredays)
@@ -147,5 +214,49 @@ function SetCookies(c_name,value,expiredays)
     document.cookie=c_name+ "=" +escape(value)+
     ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
 }
+function buscarPredio(){
+			var url = "buscarPredio.php";
+			$.get(url, mostrarPredio, 'json');
+		}
+		
+		function mostrarPredio(dados){
+			$("#selpredio").empty();
+			$("#selpredio").append(new Option("Prédio", "") );
+			$.each(dados, function(indice, linha){
+				$("#selpredio").append(new Option(linha.nome, linha.valor) );
+			});
+			
+		}
+buscarPredio();
+
+function buscarSala(){
+                        var url = "buscatudo.php?predio=" +$( "#selpredio" ).val()+"&departamento="+$( "#seldep" ).val();
+                        $.get(url, mostrarSala, 'json');
+                }
+
+                function mostrarSala(dados){
+                        $("#selsala").empty();
+                        $("#selsala").append(new Option("Sala", "") );
+                        $.each(dados, function(indice, linha){
+                                $("#selsala").append(new Option(linha.nome, linha.valor) );
+                        });
+
+                }
+//buscarSala();
+
+function buscarDepartamentos(){
+			var url = "buscatudo.php?predio="+$( "#selpredio" ).val();
+			$.get(url, mostrarDepartamentos, 'json');
+		}
+		
+		function mostrarDepartamentos(dados){
+			$("#seldep").empty();
+			$("#seldep").append(new Option("Departamento", "") );
+			$.each(dados, function(indice, linha){
+				$("#seldep").append(new Option(linha.nome, linha.valor) );
+			});
+			
+		}
+ //buscarDepartamentos();
 </script>
 </html>
