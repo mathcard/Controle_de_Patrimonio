2608f9d80ec8973de25b2a29571c479a88609382
mathcard <matheus.carmo@hotmail.com>
2017-11-21 12:34:51 -0300

    exclusões

--- excluirDepartamento.php
+++ excluirDepartamento.php
@@ -5,25 +5,31 @@ if($tipo != "P"){
 	echo "Você não tem permissão para excluir departamento";
 	header ("refresh:3; url=index.php");
 }else{
+	$valid = true;
 	$id = $_GET['id'];
-	$sala = $con->query("select numero from sala inner join departamento on sala.sigladpto = departamento.sigla where departamento.sigla = '$id'");
-	$row = $sala->fetch(PDO::FETCH_OBJ);
-        $aux = $row->numero;
-	if(!empty($aux)){
+	$sqlSala = "select numero from sala inner join departamento on sala.sigladpto = departamento.sigla where departamento.sigla = '$id'";
+	$resultado = $con->prepare($sqlSala);
+        $resultado->execute();
+	if($resultado->rowCount()> 0 ){
+		$valid = false;
 		echo "Existem <b>salas</b> cadastradas nesse departamento.<br>";
 		header ("refresh:5; url=T04.php");
 	}
- 	$user = $con->query("select id from usuario inner join departamento on usuario.sigla = departamento.sigla where departamento.sigla = '$id'");
-        $row = $user->fetch(PDO::FETCH_OBJ);
-        $aux2 = $row->id;
-        if(!empty($aux2)){
-                echo "Existem <b>usuários</b> cadastrados no departamento.<br>";
+ 	$sqlUser = "select id from usuario inner join departamento on usuario.sigla = departamento.sigla where departamento.sigla = '$id'";
+        $resultado2 = $con->prepare($sqlUser);
+	$resultado2->execute();
+        if($resultado2->rowCount()> 0 ){
+		$valid = false;
+		echo "Existem <b>usuários</b> cadastrados no departamento.<br>";
                 header ("refresh:5; url=T04.php");
-	}else{	
-	$sql = $con->prepare("delete from departamento where sigla = ?");
+	}
+	if($valid!=true){
+		echo "Não foi possivel excluir este <b>Departamento.</b>";
+	}else{
+		$sql = $con->prepare("delete from departamento where sigla = ?");
 			$sql->bindParam(1,$id);
 			$sql->execute();
-		echo "Excluido com Sucesso!!!";
-		header ("location: T04.php");
+		echo "<b>Departamento</b> Excluido com Sucesso!!!";
+		header ("refresh:5; url=T04.php");
 		}
 	}
--- excluirSala.php
+++ excluirSala.php
@@ -5,10 +5,42 @@ if($tipo != "P"){
 	echo "Você não tem permissão para excluir sala";
 	header ("refresh:3; url=index.php");
 }else{
-$id = $_GET['id'];
-$sql = $con->prepare("delete from sala where numero = ?");
-$sql->bindParam(1,$id);
-$sql->execute();
-header ("location: T05.php");
+	$valid = true;
+	$id = $_GET['id'];
+	 $sqlBem = "select b.numero as num from bempatrimonial b inner join sala on sala.numero = b.numsala where sala.numero = $id";
+	$resultado = $con->prepare($sqlBem);
+        $resultado->execute();
+	if($resultado->rowCount()> 0 ){
+		$valid = false;
+                echo "Existem <b>Bens</b> cadastrados nessa sala.<br>";
+                header ("refresh:5; url=T05.php");
+        }
+        $sqlMbp ="select m.numero as numero from mbp m inner join sala on sala.numero = m.numsalaorigem where sala.numero = '$id'";
+        $resultado2 = $con->prepare($sqlMbp);
+	$resultado2->execute();
+	if($resultado2->rowCount()>0){
+		$valid = false;
+                echo "Existem <b>Movimentações</b> registradas com essa sala <b>Origem</b>.<br>";
+                header ("refresh:5; url=T05.php");
+        }
 
+        $sqlMbpD ="select m.numero as numero from mbp m inner join sala on sala.numero = m.numsaladestino where sala.numero = '$id'";
+	$resultado3 = $con->prepare($sqlMbpD);
+          $resultado3->execute();
+          if($resultado3->rowCount()>0){
+                  $valid = false;
+                  echo "Existem <b>Movimentações</b> registradas com essa sala<b> Destino.</b><br>";
+                  header ("refresh:5; url=T05.php");
+		}
+
+	if(($valid!=true)){
+		echo "Não foi possivel excluir esta <b>sala</b>.";
+                header ("refresh:5; url=T05.php");
+	}else{
+	$sql = $con->prepare("delete from sala where numero = ?");
+	$sql->bindParam(1,$id);
+	$sql->execute();
+        echo " <b>Sala</b> excluida com sucesso!!!<br>";
+        header ("refresh:5; url=T05.php");
+	}
 }
