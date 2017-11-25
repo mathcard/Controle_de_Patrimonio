a68cd44dcf5f4b5f9e5b6d028f04a7b1410c088f
cristrauss <31821185+cristrauss@users.noreply.github.com>
2017-11-25 18:23:57 -0200

    pdf mais completo

--- pdf.php
+++ pdf.php
@@ -4,10 +4,40 @@ require "connect.php";
 $data10 = $_GET['data10'];
 $mm = $_GET['mm'];
 
-if(empty($_GET['ordem'])){
+if(!empty($_GET['ordem'])){
     $ordem = $_GET['ordem'];
 }else{
     $ordem="";
+}
+
+if(!empty($_GET['predio'])){
+    $predioS = $_GET['predio'];
+    $predio=" and sala.codpredio=" . $_GET['predio'];
+}else{
+    $predio="";
+    $predioS="";
+}
+
+if(!empty($_GET['departamento'])){
+    $departamentoS = $_GET['departamento'];
+    $departamento=" and sala.sigladpto='" . $_GET['departamento'] . "'";
+}else{
+    $departamento="";
+    $departamentoS="";
+}
+
+if(!empty($_GET['sala'])){
+    $salaS = $_GET['sala'];
+    $sala=" and sala.numero=" . $_GET['sala'];
+}else{
+    $sala="";
+    $salaS="";
+}
+
+if(!empty($_GET['nome'])){
+    $nome = $_GET['nome'];
+}else{
+    $nome="";
 }
 
 class Documento1 extends FPDF
@@ -37,17 +67,35 @@ class Documento1 extends FPDF
         $this->SetFont('Arial', 'B', 8);
         $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
 
+
     }
 }
 
 $pdf = new Documento1('P', 'pt', 'A4');
 $pdf->AddPage('L');
 $pdf->AliasNbPages();
-$sqlX= "SELECT * FROM bempatrimonial where numero in
-            ((select b.numero from bempatrimonial b where b.situacao = 'I' and b.datacompra <= '{$data10}')
-            union all
-            (select ba.numero from baixabempatrimonial ba where ba.data {$mm} '{$data10}'))" . $ordem;
 
+
+if(!empty($_GET['nome'])){
+
+    $nome = "%" . $_GET['nome'] . "%";
+   $sqlX= "SELECT bp.numero, bp.descricao, bp.datacompra, bp.prazogarantia, bp.nrnotafiscal, bp.fornecedor, bp.valor, bp.situacao, bp.codcategoria, bp.numsala from sala, bempatrimonial bp where bp.numsala=sala.numero and bp.numero in
+   ((select b.numero from bempatrimonial b where b.situacao = 'I' and b.datacompra <= '{$data10}}')
+   union all
+   (select ba.numero from baixabempatrimonial ba where ba.data {$mm} '{$data10}')) and upper(bp.descricao) like upper('{$nome}')". $predio . $departamento . $sala . $ordem;
+ 
+    $resultado = $con->prepare($sqlX);
+    $resultado->execute();
+
+}else{
+            $sqlX="SELECT bp.numero, bp.descricao, bp.datacompra, bp.prazogarantia, bp.nrnotafiscal, bp.fornecedor, bp.valor, bp.situacao, bp.codcategoria, bp.numsala from sala, bempatrimonial bp where bp.numsala=sala.numero and bp.numero in
+            ((select b.numero from bempatrimonial b where b.situacao = 'I' and b.datacompra <= '{$data10}}')
+            union all
+            (select ba.numero from baixabempatrimonial ba where ba.data {$mm} '{$data10}'))". $predio . $departamento . $sala . $ordem;
+
+            $resultado = $con->prepare($sqlX);
+            $resultado->execute();
+            }
 
 $resultado = $con->prepare($sqlX);
 $resultado->execute();
@@ -65,7 +113,9 @@ while ($row = $resultado->fetchObject()) {
     $pdf->Cell(60, 20, $row->codcategoria, 1, 0, 'L'); 
     $pdf->Cell(60, 20, $row->numsala, 1, 1, 'L'); 
 
-}
 
+}
+$rodape="REFERENTE A DATA: {$data10}       PREDIO:{$predioS}       DEPARTAMENTO:{$departamentoS}       SALA:{$salaS}      NOME:{$nome}";
+$pdf->Cell(800, 20, $rodape, 1,1, 'C');
 $pdf->Output();
 ?>
