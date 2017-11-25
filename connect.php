a485e1151f4ffa968b47478fb1edf62f29443b8a
cristrauss <31821185+cristrauss@users.noreply.github.com>
2017-11-25 18:21:36 -0200

    esqueci do try haha

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+try {
 $con = new PDO('pgsql:dbname=patrimonio;host=localhost;user=queops;password=piramide');
 } catch (PDOException $e) {
     echo "Problemas na conexão com o banco de dados!</br>";
